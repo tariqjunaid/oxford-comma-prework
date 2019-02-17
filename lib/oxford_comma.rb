4e85ca0e4ba5bbdc8730a4329d473c7c99d11862
Junaid Tariq <junaidtariq_91@yahoo.com>
2019-02-17 01:51:43 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,8 @@
 def oxford_comma(array)
-  if array.length = 
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
   array[-1] = "and #{array[-1]}"
   array.join(", ")
 end
