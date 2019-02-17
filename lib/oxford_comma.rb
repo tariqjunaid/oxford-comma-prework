66aa7df1a828b9ecd2713a38f60de72df45bb534
Junaid Tariq <junaidtariq_91@yahoo.com>
2019-02-17 01:51:59 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,6 +3,7 @@ def oxford_comma(array)
     array.join
   elsif array.length == 2
     array.join(" and ")
-  array[-1] = "and #{array[-1]}"
-  array.join(", ")
+  else
+    array[-1] = "and #{array[-1]}"
+    array.join(", ")
 end
